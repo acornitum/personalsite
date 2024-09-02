206dc3a26ac1ced4bbd96d9c2ffb59bb1f578c56
acon <101356372+acornitum@users.noreply.github.com>
2024-09-02 14:12:19 -0400

    debuggggh

--- src/components/mobile.js
+++ src/components/mobile.js
@@ -7,8 +7,14 @@ export default function Mobile() {
         <p className="text-2xl flex flex-col justify-center items-center pb-6">
           hi! i'm
         </p>
-        <p className="text-2xl pb-6">
+        <p className="text-9xl pb-6">
           hi! i'm
+        </p>
+        <p className="text-9xl pb-6 text-cmykMag">
+          hi! i'm
+        </p>
+        <p className="text-9xl pb-6 text-cmykMag">
+          acon
         </p>
         <p className="text-9xl flex flex-col justify-center items-center pb-6 text-cmykMag font-bold">
           aconi
